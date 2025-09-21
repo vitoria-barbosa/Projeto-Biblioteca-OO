664968d3cf75ea1fc6ed50053061a0ea6bbc8c23
Thalisson <thalissonmoura138@gmail.com>
2025-09-21 15:09:44 -0300

    :sparkles:feat:adiciona classes principais com seus respectivos atributos e metodos acessores.

---
+++ src/Usuario.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class Usuario {
+    private String nome;
+    private String matricula;
+    private List<Livro> livrosEmprestados;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public List<Livro> getLivrosEmprestados() {
+        return livrosEmprestados;
+    }
+
+    public void setLivrosEmprestados(List<Livro> livrosEmprestados) {
+        this.livrosEmprestados = livrosEmprestados;
+    }
+}
